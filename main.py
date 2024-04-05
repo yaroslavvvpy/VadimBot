884b762bff356d4957a655a62b40b58cfc3a7eef
yaroslavvvpy <132778867+yaroslavvvpy@users.noreply.github.com>
2024-04-05 16:32:32 +0300

    Update main.py
    voice record added

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import discord
 from discord.ext import commands
 from datetime import datetime
+import pyaudio
+import wave
 
 intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
+intents.voice_states = True
 
 
 TOKEN = 'MTIyNDc3MTczMjM0MzM2MTYwNg.GoyBcY.aM605ZxwwkD2HPVHbcdQz7lashlxvlUCQG2e-Q'
@@ -53,6 +56,62 @@ async def урок(ctx, action: str):
             await ctx.send(f"Урок закончился! Продолжительность урока: {minutes} минут {seconds} секунд.")
         else:
             await ctx.send("Бот не был подключен к голосовому каналу.")
+
+
+async def record_audio(ctx):
+    # Подключение к голосовому каналу автора команды
+    voice_channel = ctx.author.voice.channel
+    vc = await voice_channel.connect()
+
+    # Начало записи аудио
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 1
+    RATE = 44100
+    CHUNK = 1024
+    RECORD_SECONDS = 10
+    WAVE_OUTPUT_FILENAME = "output.wav"
+
+    audio = pyaudio.PyAudio()
+
+    stream = audio.open(format=FORMAT, channels=CHANNELS,
+                        rate=RATE, input=True,
+                        frames_per_buffer=CHUNK)
+
+    print("Recording...")
+
+    frames = []
+
+    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+        data = stream.read(CHUNK)
+        frames.append(data)
+
+    print("Finished recording.")
+
+    stream.stop_stream()
+    stream.close()
+    audio.terminate()
+
+    # Сохранение записанного аудио на компьютере
+    wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(audio.get_sample_size(FORMAT))
+    wf.setframerate(RATE)
+    wf.writeframes(b''.join(frames))
+    wf.close()
+
+    # Отключение от голосового канала
+    await vc.disconnect()
+
+    # Отправка сообщения с информацией о файле
+    await ctx.send("Аудиозапись сохранена!")
+
+# Команда для запуска записи аудио
+@bot.command()
+async def start_recording(ctx):
+    if ctx.author.voice:
+        await record_audio(ctx)
+    else:
+        await ctx.send("Вы должны находиться в голосовом канале для использования этой команды.")
 
 
 bot.run(TOKEN)
